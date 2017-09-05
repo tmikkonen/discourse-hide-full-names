b21a1fac041b5c58e9ff31950e7cf98143548be6
tomikkon <tomi.mikkonen@helsinki.fi>
2017-09-05 17:28:28 +0300

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-hide-full-names
+# about: Hide full names from bots & anonymous users 
+# version: 0.1
+# authors: tmikkonen
+# url: https://github.com/tmikkonen/discourse-hide-full-names
+
+enabled_site_setting :hide_full_names_without_login_enabled
