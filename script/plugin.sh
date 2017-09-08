d0f01b7d3999d4410bdf22c462f94a2f2f9b216e
tomikkon <tomi.mikkonen@helsinki.fi>
2017-09-08 20:59:05 +0300

    Fix script variables

--- script/plugin.sh
+++ script/plugin.sh
@@ -3,18 +3,18 @@
 # Copy a Discourse core file to the plugin directory and modify it accordingly
 
 DISCOURSE=/var/www/discourse
-PATH=assets/javascripts/discourse/widgets
-FILE=poster-name.js.es6
+D_PATH=assets/javascripts/discourse/widgets
+D_FILE=poster-name.js.es6
 PLUGIN=discourse-hide-full-names
-PLUGIN_PATH=$DISCOURSE/plugins/$PLUGIN/$PATH
-PLUGIN_FILE=$PLUGIN_PATH/$FILE
+PLUGIN_PATH=$DISCOURSE/plugins/$PLUGIN/$D_PATH
+PLUGIN_FILE=$PLUGIN_PATH/$D_FILE
 
 # check if the file already exists
 if [ -f "$PLUGIN_FILE" ]; then
   rm $PLUGIN_FILE
 fi
-mkdir $PLUGIN_PATH
-cp $DISCOURSE/app/$PATH/$FILE $PLUGIN_FILE
+mkdir -p $PLUGIN_PATH
+cp $DISCOURSE/app/$D_PATH/$D_FILE $PLUGIN_FILE
 
 # modify the file if it hasn't been modified already
 if ! grep -q hide_full_names_without_login_enabled $PLUGIN_FILE; then
